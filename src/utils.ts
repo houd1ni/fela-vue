93288c7dd58a61133eaad5d9bafdd22bc8c61f15
Michael <akiliev@outlook.com>
2019-04-14 19:18:52 +0300

    lint fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { AnyObject } from "./types"
-
 export const always = (a: any) => () => a
 export const reflect = (a: any) => a
 export const camelify = (str: string) => str.replace(/-(\w)/gu, (_s, l) => l.toUpperCase())
